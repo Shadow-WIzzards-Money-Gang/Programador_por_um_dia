a4143d23a84e9a6f7148481a54b95def1ee70a99
Antonio <antoniojacintoa@gmail.com>
2025-10-31 09:55:42 -0300

    feat: criando componente header no frontend

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = ({ usuario, setUsuario }) => {
+  return (
+    <header className="flex justify-between p-5 w-full bg-red-400">
+        <nav className="">
+            <ul>
+
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
