55ae1152fdeb0e44eea4207a764e5e6f66117a35
Antonio <antoniojacintoa@gmail.com>
2025-11-02 14:43:48 -0300

    feat: adicionando rotas home, sobre, profutos, login no frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Header from "./components/Header"
+import Home from './routes/Home';
+import Sobre from './routes/Sobre';
+import Produtos from './routes/Produtos';
+import Login from './routes/Login';
 
 function App() {
   return (
     <section className="bg-white min-h-screen">
-      <Header />      
+      <Header />    
+      <main>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/sobre' element={<Sobre />}/>
+            <Route path='/produtos' element={<Produtos />}/>
+            <Route path='/login' element={<Login />}/>
+          </Routes>
+        </BrowserRouter>
+      </main>  
     </section>
   )
 }
